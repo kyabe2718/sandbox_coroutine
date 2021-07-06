b57881575a3ed41697760aa4f3f01fa62b308305
kyabe2718 <kyabetsu.3141@gmail.com>
2021-07-07 00:26:01 +0900

    update

--- test/task.cpp
+++ test/task.cpp
@@ -61,33 +61,4 @@ TEST_CASE("task of reference type") {
         }
     }());
 }
-
 TEST_SUITE_END();
-
-//int main() {
-//    static_assert(coro::Awaitable<coro::task<void>>);
-//
-//    auto func = []() -> coro::task<int> {
-//        std::cout << "func" << std::endl;
-//        std::this_thread::sleep_for(1s);
-//        co_return 10;
-//    };
-//
-//    auto task = [&]() -> coro::task<int> {
-//        auto t = func();
-//
-//        std::cout << "start" << std::endl;
-//        int v = co_await t;
-//        std::cout << v << std::endl;
-//        std::cout << "end " << std::endl;
-//
-//        co_return 10;
-//    };
-//
-//    // task().resume();
-//
-//    auto do_task = [&]() -> coro::generator<int> {
-//        co_yield co_await task();
-//    }();
-//    do_task();
-//}
